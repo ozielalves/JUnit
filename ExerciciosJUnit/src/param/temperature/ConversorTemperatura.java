f5464132506311cf5125b04ebc240c9509f383bc
Oziel Alves <ozielalves@ufrn.edu.br>
2020-10-07 11:22:06 -0300

    Testes Conversor de Temperaturas

--- ExerciciosJUnit/src/param/temperature/ConversorTemperatura.java
+++ ExerciciosJUnit/src/param/temperature/ConversorTemperatura.java
@@ -5,24 +5,27 @@ public class ConversorTemperatura{
   
     public ConversorTemperatura(){ }  
   
-    public Temperatura converte(Temperatura temp) throws TempException{  
+    public static Temperatura converte(Temperatura temp) throws TempException{  
        if(temp instanceof Celsius) return converteToFahrenheit(temp);  
        else return converteToCelsius(temp);  
     }  
   
-    private Temperatura converteToFahrenheit(Temperatura celsius) throws TempException{  
+    private static Temperatura converteToFahrenheit(Temperatura celsius) throws TempException{  
        Fahrenheit f = new Fahrenheit();  
        double cvalue = celsius.getValue();   
        double fvalue = 1.8*cvalue+f.getFREEZE(); // formula 1  
        f.setValue(fvalue);  
+//       System.out.println("Valor de C em F é: " + (f));
        return f;  
     }  
   
-    private Temperatura converteToCelsius(Temperatura fahrenheit) throws TempException{  
+    private static Temperatura converteToCelsius(Temperatura fahrenheit) throws TempException{  
        Celsius c = new Celsius();  
-       double fvalue = fahrenheit.getValue();   
+       double fvalue = fahrenheit.getValue();
+//       System.out.println("Valor em F recebido: " + (fvalue));
        double cvalue = (5/9)*fvalue-5*fahrenheit.getFREEZE();// formula 2 ?!  
-       c.setValue(cvalue);  
+       c.setValue(cvalue);
+//       System.out.println("Valor de F em C é: " + (c));
        return c;  
     }  
   
---
+++ ExerciciosJUnit/src_test/param/temperature/ConversorTempTest.java
@@ -0,0 +1,59 @@
+package param.temperature;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+/**
+ * @author Oziel Alves
+ * 
+ */
+@RunWith(Parameterized.class)
+public class ConversorTempTest {
+	
+	private Temperatura tc;
+	private Temperatura tf;
+	private double entradaC;
+	private double esperadoF;
+	
+	public ConversorTempTest (double inC, double espF){
+		entradaC = inC;
+		esperadoF = espF;
+	}
+	
+	@Parameters(name = "{index}: Celcius[{0}] = Farenheit [{1}]")
+	public static Object[][] data() {
+		return new Object [][] {
+			{ 0, 32},
+			{ 10, 50},
+			{ 25, 77},
+		};
+	}
+	
+	@Before
+	public void inicialize() {
+		tf = new Fahrenheit();
+		tc = new Celsius();
+	}
+	
+	@Test
+	public void ConverteCelsius() throws TempException {
+		tc.setValue(entradaC);
+		Temperatura f = ConversorTemperatura.converte(tc);
+		assertTrue(f.getValue() == esperadoF);
+	}
+	
+	@Test
+	public void ConverteFarenheit() throws TempException {
+		tf.setValue(esperadoF);
+		Temperatura c = ConversorTemperatura.converte(tf);
+		assertTrue(c.getValue() == entradaC);
+	}
+	
+}
+
